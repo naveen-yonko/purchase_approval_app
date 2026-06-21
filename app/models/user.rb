a4c10aa7d11693b6b7b91667a40b69de82e7fc05
naveenyonko <naveenyorichi@gmail.com>
2026-06-21 23:21:37 +0530

    Day 12 exercise: Add user status enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
     approver: 1,
     admin: 2
   }
-
+  enum :status, {
+    active: 0,
+    inactive: 1,
+    suspended: 2
+  },prefix: true
+#without prefix, it will create methods like active?, inactive?, suspended? but it will cause conflict with others enums if they have same name to solve this we are using that prefix which makes the methods like status_active?, 
   validates :name, presence: true
 
   validates :email,
@@ -12,4 +17,20 @@ class User < ApplicationRecord
             uniqueness: true
 
   validates :role, presence: true
+
+  validates :status, presence: true
 end
+
+# we can also solve this without prefix by using this insted of prefix   instance_methods: false
+# and by using custom methods
+=begin
+def active?
+    status == "active"
+  end
+
+  def inactive?
+    status == "inactive"
+  end
+
+  we can now call like this User.active? 
+  =end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_19_170559) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_21_171944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_19_170559) do
     t.string "email"
     t.string "name"
     t.integer "role"
+    t.integer "status", default: 0, null: false
     t.datetime "updated_at", null: false
   end
 end
